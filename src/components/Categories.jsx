81a9978b5f00ebad9ecb69816607ddd328cd13e0
tahmid-khan-hub <tahmidkhan0011@gmail.com>
2025-05-01 17:49:03 +0600

    fixed the left side styles and data load

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,4 +1,5 @@
 import React, { use } from 'react';
+import { NavLink } from 'react-router';
 
 const promise = fetch("/categories.json")
 .then((res) => res.json())
@@ -11,6 +12,11 @@ const Categories = () => {
     return (
         <div>
             <h2 className='font-bold'>All Categories</h2>
+            <div className='grid grid-cols-1 mt-5 gap-3'>
+                {
+                    categories.map(category => <NavLink className={"btn bg-base-100 border-0 hover:bg-base-200 font-semibold text-accent"} key={category.id} to={`/category/${category.id}`}>{category.name}</NavLink>)
+                }
+            </div>
         </div>
     );
 };
--- src/pages/CategoryNews.jsx
+++ src/pages/CategoryNews.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { useParams } from 'react-router';
 
 const CategoryNews = () => {
+    const {id} = useParams();
+    console.log(id);
     return (
         <div>
-            category
+            category - {id}
         </div>
     );
 };
