708187c38612180e507382d82d9a046877a626d8
tahmid-khan-hub <tahmidkhan0011@gmail.com>
2025-05-03 01:28:56 +0600

    added private routes and loading

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -6,6 +6,7 @@ import Login from "../pages/Login";
 import Register from "../pages/Register";
 import AuthLayout from "../layouts/AuthLayout";
 import NewsDetails from "../pages/NewsDetails";
+import PrivateRoute from "../provider/PrivateRoute";
 
 const router = createBrowserRouter(
     [
@@ -40,7 +41,9 @@ const router = createBrowserRouter(
         },
         {
             path: '/news-details/:id',
-            element: <NewsDetails></NewsDetails>,
+            element: <PrivateRoute>
+                    <NewsDetails></NewsDetails>
+                    </PrivateRoute>,
             loader: ()=> fetch("/news.json")
         },
         {
--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -8,15 +8,18 @@ const auth = getAuth(app)
 
 const AuthProvider = ({children}) => {
 
-    const [user, setUser] = useState(null)
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
 
     console.log(user);
 
     const createUser = (email, password) =>{
+        setLoading(true)
         return createUserWithEmailAndPassword(auth, email, password);
     }
 
     const signIn = (email, password) =>{
+        setLoading(true)
         return signInWithEmailAndPassword(auth, email, password);
     }
 
@@ -28,7 +31,8 @@ const AuthProvider = ({children}) => {
 
     useEffect(()=>{
         const unsubscribe = onAuthStateChanged(auth, (currentUser)=>{
-            setUser(currentUser)
+            setUser(currentUser);
+            setLoading(false)
         })
         return()=>{
             unsubscribe();
@@ -41,6 +45,8 @@ const AuthProvider = ({children}) => {
         createUser,
         logOut,
         signIn,
+        loading,
+        setLoading,
     }
 
     return <AuthContext value={authData}>{children}</AuthContext>
